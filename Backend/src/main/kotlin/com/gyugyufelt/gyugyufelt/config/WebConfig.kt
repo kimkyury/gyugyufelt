641c22502af224712cfb3c4daddce417d00c26b0
김규리 <mandarining0918@gmail.com>
2025-05-11 16:04:27 +0900

    chore: Add WebConfig for local

---
+++ Backend/src/main/kotlin/com/gyugyufelt/gyugyufelt/config/WebConfig.kt
@@ -0,0 +1,17 @@
+package com.gyugyufelt.gyugyufelt.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:5173") // local
+            .allowedHeaders("*")
+            .allowedMethods("GET", "PSOT", "PUT", "DELETE")
+            .allowCredentials(true)
+    }
+}
