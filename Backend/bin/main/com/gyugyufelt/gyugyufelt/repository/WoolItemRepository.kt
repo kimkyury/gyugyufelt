22a3d94913de557cc1d727a8af901d689ccb89b1
KGYURY <71677810+kimkyury@users.noreply.github.com>
2025-05-11 16:58:49 +0900

    Revert "Feature/frontend"

--- Backend/bin/main/com/gyugyufelt/gyugyufelt/repository/WoolItemRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.gyugyufelt.gyugyufelt.repository
-
-import com.gyugyufelt.gyugyufelt.domain.WoolItem
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface WoolItemRepository : JpaRepository<WoolItem, Long>
